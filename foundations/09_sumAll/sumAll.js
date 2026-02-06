180747e4d6fb7a24a8cc72df1547e65d006c9c8e
ga11ego <og61255@gmail.com>
2026-02-06 14:11:46 -0500

    09_sumAll activity done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    let result = 0; 
+    if ( Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0){
+        if ( a < b ){
+            for (let i = a; i <= b; i++){
+                result += i;
+            } 
+        } else {
+            for (let i = b; i <= a; i++){
+                result += i;
+            } 
+        }
+    } else{
+        return "ERROR"
+    }
+
+    return result
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+console.log(sumAll(2,4))
