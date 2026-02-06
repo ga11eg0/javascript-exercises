0cc51e39768f9e8448d7631f3d26c1bee11a626c
ga11ego <og61255@gmail.com>
2026-02-06 12:14:05 -0500

    07_reverseString activity done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let result = "";
+
+    for ( let i = string.length-1 ; i >= 0; i-- ){
+        result += string[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//console.log(reverseString("hello world"));
