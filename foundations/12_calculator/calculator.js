cd370eee243df6cdb00a9c08b99356a17a7bc790
ga11ego <og61255@gmail.com>
2026-03-12 13:57:20 -0500

    activity 12-calculator done

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	result = arr.reduce((total,value) => total+value,0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = arr.reduce((total,value) => total*= value,1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base,expo) {
+	return base**expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	facto = 1;
+  for (i = 1; i <= number;i++){
+    facto *= i;
+  };
+  return facto;
 };
 
 // Do not edit below this line
