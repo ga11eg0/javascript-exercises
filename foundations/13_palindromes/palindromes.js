08a232be87fdcb5231940290621095504093c878
ga11ego <og61255@gmail.com>
2026-03-13 14:31:24 -0500

    all foundations exercises done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,8 +1,11 @@
 const palindromes = function (phrase) {
+    // remove all character that we wont need; " " , . ! ; joint it back and convert to lowercase
     let clean = phrase.split(/[ ,.!]+/).join("").toLowerCase();
     console.log("clean: ",clean,", length: ", clean.length);
+    // reverse the cleaned string 
     let revers = [...clean].reverse().join("");
     console.log("rev: ",revers," leng: ", revers.length);
+    // compare the cleaned version with the reversed one 
     return clean === revers;
 };
 
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    // get a sorted array from younger to oldest 
+    const oldest = arr.sort( (a,b ) => {
+        
+        if (a.yearOfDeath && b.yearOfDeath){
+        return (a.yearOfDeath-a.yearOfBirth)-(b.yearOfDeath-b.yearOfBirth);
+        }
+        else if (a.yearOfDeath === undefined){
+            return (2026-a.yearOfBirth)-(b.yearOfDeath-b.yearOfBirth);
+        } else if (b.yearOfDeath === undefined){
+            return (a.yearOfDeath-a.yearOfBirth)-(2026-b.yearOfBirth);
+        }
+    } );
+
+    // return the las object from sorted arr (the last element is the oldest)
+    return oldest[oldest.length-1]
 };
 
 // Do not edit below this line
